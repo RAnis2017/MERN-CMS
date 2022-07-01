717f27a653110102891e167abba81bc35a33bb72
Raza Anis <razaanis@Razas-MacBook-Pro.local>
2022-07-02 02:01:43 +0500

    components made

--- src/App.js
+++ src/App.js
@@ -16,22 +16,6 @@ function App(props) {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/admin">About</Link>
-            </li>
-            <li>
-              <Link to="/posts">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Routes>
           <Route path="/" element={ <Login/> } />
           <Route path="admin" element={ <Admin/> } />
